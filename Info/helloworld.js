7a64a50e558d25a0d0fe774eda6de7447d907cfc
Vo Tuan <votuanbk051@gmail.com>
2020-02-12 18:11:22 +0700

    update code

--- Info/helloworld.js
+++ Info/helloworld.js
@@ -1,5 +1,10 @@
 "use strict";
+const alert = require('alert-node');
 
-setTimeout(function () {
-  console.log(null);
-}, 200);
+let s1 = 'S\u0307\u0323'; // Ṩ, S + dot above + dot below
+let s2 = 'S\u0307\u0323'; // Ṩ, S + dot below + dot above
+
+console.log( `s1: ${s1}, s2: ${s2}` );
+
+console.log('S\u0307\u0323'); // false though the characters look identical (?!)
+console.log('\u1e68'); // false though the characters look identical (?!)

cc2fd8fc2efb72369e6b9ef53c2075cd40925876
Vo Tuan <votuanbk051@gmail.com>
2020-02-14 19:44:47 +0700

    update

--- Info/helloworld.js
+++ Info/helloworld.js
@@ -1,10 +1,21 @@
 "use strict";
 const alert = require('alert-node');
 
-let s1 = 'S\u0307\u0323'; // Ṩ, S + dot above + dot below
-let s2 = 'S\u0307\u0323'; // Ṩ, S + dot below + dot above
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  
+  get area() {
+    return this.calcArea();
+  }
 
-console.log( `s1: ${s1}, s2: ${s2}` );
+  calcArea() {
+    return this.height * this.width;
+  }
+}
 
-console.log('S\u0307\u0323'); // false though the characters look identical (?!)
-console.log('\u1e68'); // false though the characters look identical (?!)
+const square = new Rectangle(10, 10);
+
+console.log(square.area);

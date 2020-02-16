4dceb4071a180de83e968e9e0dba0216d5cfa743
Vo Tuan <votuanbk051@gmail.com>
2020-02-16 18:43:42 +0700

    update code

--- Info/helloworld.js
+++ Info/helloworld.js
@@ -1,21 +1,23 @@
 "use strict";
 const alert = require('alert-node');
+const _ = require('lodash');
 
-class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
+const data = [{
+    "id": 52,
+    "carBodyKindId": 11,
+    "carMakerCarKindId": 238,
+    "name": "Ｊ１００トラック　１．５ｔ　２ＷＤ",
+  },
+  {
+    "id": 52,
+    "carBodyKindId": 11,
+    "carMakerCarKindId": 238,
+    "name": "Ｊ１００トラック　１．５ｔ　２ＷＤ",
   }
-  
-  get area() {
-    return this.calcArea();
-  }
+]
 
-  calcArea() {
-    return this.height * this.width;
-  }
-}
-
-const square = new Rectangle(10, 10);
-
-console.log(square.area);
+console.log(_.map(data, x => {
+  return _.assign(x, {
+    parent: 1
+  });
+}));

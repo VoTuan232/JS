15aa7702c3a613c3a607ec570c2259bfb1cb1520
votuan <votuanbk2302@gmail.com>
2020-03-08 21:20:38 +0700

    update

---
+++ Info/function_practice/loop/readme.md
@@ -0,0 +1,5 @@
+For ...in: get key, lặp trên tất cả các thuộc tính ( ko chỉ riêng số: có độ dài và chỉ mục index)
+For ...of: get value(new ES 6), ko lặp trên các đối tượng đặc biệt
+
+For...in tối ưu hóa cho các object, ko pải array => chậm hơn 10 tới 100 lần. nhưng vẫn rất nhanh
+==> ko nên sử dụng for...in cho mảng
---
+++ Info/function_practice/reduce/return_function.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function increment(input) { return input + 1; }
+
+function decrement(input) { return input + -1; }
+
+function double(input) { return input * 2; }
+
+function halve(input) { return input / 2; }
+
+let pipeline = [increment, double, decrement];
+
+const result = pipeline.reduce((total, func) => {
+    return func(total);
+}, 1);
+
+console.log(result)

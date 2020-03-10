d3cb51733a87bdc2ee9d1d598262202f89db968b
votuan <votuanbk2302@gmail.com>
2020-03-10 21:31:01 +0700

    update

--- Info/codesignal/simplifyPath.js
+++ Info/codesignal/simplifyPath.js
@@ -8,12 +8,24 @@
 
 let simplifyPath = path => {
   path = removeSpecial(path, [
-    { o: "//", d: "/", reg: new RegExp(/\/\//g, "g") },
-    { o: "/.", d: "", reg: new RegExp(/\/\./g, "g") }
+    { o: "//", d: "/", reg: new RegExp(/\/\//, "g") },
+    { o: "..", d: "!", reg: new RegExp(/\.\./, "g") },
+    { o: "/.", d: "", reg: new RegExp(/\/\./, "g") }
   ]);
 
-  console.log(path);
-  return path;
+  let arr = path.split("/").filter(x => x !== "")
+  while (arr.indexOf('!') !== -1) {
+    let i = arr.indexOf('!');
+    // console.log('i')
+    // console.log(i)
+    i > 0 ? arr.splice(i - 1, 2) : arr.splice(0, 1)
+    // console.log('arr');
+    // console.log(arr);
+  }
+  // console.log(arr);
+  console.log(`/${arr.join("/")}`)
+
+  return `/${arr.join("/")}`;
 };
 
 let removeSpecial = (string, characters = []) => {
@@ -38,10 +50,11 @@ let removeSpecial = (string, characters = []) => {
 // console.log(simplifyPath("/../")); //      "/"
 // console.log(simplifyPath("//a//b//./././c")); //   /a/b/c
 // console.log(simplifyPath("a/../../b/")); //   /b
-// console.log(simplifyPath("a/b/../c/d/../../e/../../")); //   /"/"
+// console.log(simplifyPath("a/b/../c/d/../../e/../../")); //   "/"
 
-simplifyPath("//a//b//./././c");
+simplifyPath("a/b/../c/d/../../e/../../");
 // console.log("/./a".replace("/.", ""));
 // console.log("/a/b/./././c".replace(/\/\./g, "1"));
-// console.log("/a/b/./././c".replace(new RegExp(/\/\./g, "g"), "1"));
+// console.log("/a/b/./././c".replace(new RegExp(/\/\./, "g"), "1"));
 // console.log(new RegExp("//", "g"));
+// console.log("123".indexOf("2"))

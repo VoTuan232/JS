5329e1f67e97da1fe1950405de8e29b48135c964
tuan.voanh <tuan.voanh@vietis.com.vn>
2020-03-20 18:38:12 +0700

    update

--- Info/index.js
+++ Info/index.js
@@ -1,9 +1,21 @@
-function sayHi() {
-  phrase = "Hello"; // (*)
-
-  if (true) {
-    var phrase;
-  }
-  alert(phrase);
+function delay() {
+  // setTimeout(() => {
+  //   console.log("run");
+  // }, 1000);
+  return new Promise(resolve => setTimeout(resolve, 1000));
 }
-sayHi();
+
+async function delayedLog(item) {
+  // notice that we can await a function
+  // that returns a promise
+  await delay();
+  console.log(item);
+}
+async function processArray(array) {
+  for (const item of array) {
+    await delayedLog(item);
+  }
+  console.log("Done!");
+}
+
+processArray([1, 2, 3]);
